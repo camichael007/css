6458708410df8c7bce01b2408c32530b0f299c1c
刀仔 <daozai@juanpi.com>
2017-06-27 15:09:18 +0200

    提交output.js

---
+++ static/js/output.js
@@ -0,0 +1,14 @@
+/**
+ * Created by daozai on 2017/6/27.
+ */
+
+function myFunction() {
+    x = 9;
+    y = 10;
+    c = (x + y) * 10;
+    return document.getElementById('demo').innerHTML = c;
+}
+
+myFunction();
+
+
